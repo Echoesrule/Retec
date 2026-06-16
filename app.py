dd3d589c47bcc90a2cb0192b5ad0e170d7217029
Echoesrule <kipronoemmanuel911@gmail.com>
2026-06-16 20:30:20 +0300

    Fix hero video not showing by excluding static files from no_cache handler; update contact info and social links

--- app.py
+++ app.py
@@ -231,6 +231,8 @@ def inject_globals():
 
 @app.after_request
 def no_cache(response):
+    if request.path.startswith('/static/'):
+        return response
     response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
     response.headers['Pragma'] = 'no-cache'
     response.headers['Expires'] = '0'
